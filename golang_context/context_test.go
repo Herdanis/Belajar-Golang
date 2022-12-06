6cdf8bbbe9860d9ae1d4e0b54eff164cc1586c75
Herdanis <herdanis.pradana@gmail.com>
2022-12-06 09:43:15 +0700

    Update Golang Context

---
+++ golang_context/context_test.go
@@ -0,0 +1,77 @@
+package golangcontext
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func TestContext(t *testing.T) {
+
+	bg := context.Background()
+	fmt.Println(bg)
+
+	todo := context.TODO()
+	fmt.Println(todo)
+}
+
+func TestContextWithValue(t *testing.T) {
+	contextA := context.Background()
+
+	contextB := context.WithValue(contextA, "a", "B")
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+
+	contextF := context.WithValue(contextC, "f", "F")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+
+	fmt.Println(contextF.Value("f")) // dapat
+	fmt.Println(contextF.Value("c")) // dapat milik parent
+	fmt.Println(contextF.Value("b")) // tidak dapat, beda parent
+	fmt.Println(contextA.Value("c")) // tidak bisa mengambil child
+}
+
+func CreateCounter(ctx context.Context) chan int {
+	destination := make(chan int)
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+			}
+		}
+	}()
+
+	return destination
+}
+
+func TestContextWithCancel(t *testing.T) {
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+	parent := context.Background()
+	ctx, cancel := context.WithCancel(parent)
+	destination := CreateCounter(ctx)
+	for n := range destination {
+		fmt.Println("Counter", n)
+		if n == 10 {
+			break
+		}
+	}
+	cancel()
+	time.Sleep(2 * time.Second)
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+}
